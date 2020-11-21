5baba592e8c94e83b4200d8af895a53df4392927
Ben Sands <m3gap0ky0@gmail.com>
2020-11-21 14:06:27 -0700

    finished exercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,75 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 1, 3)]
+        [InlineData(7, 5, 0, 12)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 3, 7)]
+        [InlineData(5, 0, 5)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5, 5, 25)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(8, 2, 4)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void HelloWorld()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            string actual = test.HelloWorld();
             //Assert
-
+            Assert.Equal("Hello World!", actual);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void OnePlusOne()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            int actual = test.OnePlusOne();
             //Assert
+            Assert.Equal(2, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,24 +3,49 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // DONE Make sure your references are set properly
+        // DONE Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+            //throw new NotImplementedException();
+        }
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+            //throw new NotImplementedException();
+        }
+        // Keep track of which number is getting passed as minuend and subtrahend
 
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+            //throw new NotImplementedException();
+        }
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+           // throw new NotImplementedException();
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
-
-        
+        public string HelloWorld()
+        {
+            return "Hello World!";
+            //throw new NotImplementedException();
+        }
+        public int OnePlusOne()
+        {
+            return 1 + 1;
+            //throw new NotImplementedException();
+        }
     }
 }
